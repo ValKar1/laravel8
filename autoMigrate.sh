29ebe0ad35cd61626222f858d82a3f4b433fc034
Valerius Karch <valerius.karch@adversign-media.de>
2020-12-06 19:34:02 +0100

    UPDATED: autoMigrate script

--- autoMigrate.sh
+++ autoMigrate.sh
@@ -3,27 +3,26 @@
 # Display status
 php artisan migrate:status
 
-ran="| Yes  |";
-
 # Count migrations before git pull
 migrationsStatus=$(php artisan migrate:status)
+ran="| Yes  |";
 countMigrationsBefore=`grep -o "$Ran" <<< "$migrationsStatus" | wc -l`
-echo "Ran migrations: "$countMigrations;
 
 git pull 
 
 # Count migrations after git pull
 migrationsStatus=$(php artisan migrate:status)
+ran="| Yes  |";
 countMigrationsAfter=`grep -o "$Ran" <<< "$migrationsStatus" | wc -l`
-echo "Ran migrations: "$countMigrationsAfter;
 
 # Get difference
 difference=$(( $countMigrationsBefore - $countMigrationsAfter ))
-echo "Difference: "$difference
+echo "migrationsBefore - migrationsAfter = difference"
+echo $countMigrationsBefore" - "$countMigrationsAfter" = "$difference
 
 # Detect rollback
 if [ $difference < 0 ]; then
-    echo "php artisan migrate:rollback --step="$difference;
+    echo "php artisan migrate:rollback --step="${difference#-}
     php artisan migrate:rollback --step=$difference
     # Migrate to safety
     php artisan migrate
