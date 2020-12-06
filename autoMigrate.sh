e10fff782e2c26fc160c3060cfb01e75cfec76b0
Valerius Karch <valerius.karch@adversign-media.de>
2020-12-06 19:27:45 +0100

    UPDATED: posts table

--- autoMigrate.sh
+++ autoMigrate.sh
@@ -3,20 +3,21 @@
 # Display status
 php artisan migrate:status
 
+ran="| Yes  |";
+
 # Count migrations before git pull
 migrationsStatus=$(php artisan migrate:status)
-ran="| Yes  |";
 countMigrationsBefore=`grep -o "$Ran" <<< "$migrationsStatus" | wc -l`
 echo "Ran migrations: "$countMigrations;
 
 git pull 
 
-# Count migrations before git pull
+# Count migrations after git pull
 migrationsStatus=$(php artisan migrate:status)
-ran="| Yes  |";
 countMigrationsAfter=`grep -o "$Ran" <<< "$migrationsStatus" | wc -l`
 echo "Ran migrations: "$countMigrationsAfter;
 
+# Get difference
 difference=$(( $countMigrationsBefore - $countMigrationsAfter ))
 echo "Difference: "$difference
 
