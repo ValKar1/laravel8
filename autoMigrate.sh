8a8e0271d4ce0111707bdf1680c80d3f5cb67e78
Valerius Karch <valerius.karch@adversign-media.de>
2020-12-06 19:23:55 +0100

    UPDATED: automigrate shell script

--- autoMigrate.sh
+++ autoMigrate.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+# Display status
 php artisan migrate:status
+
+# Count migrations before git pull
 migrationsStatus=$(php artisan migrate:status)
 ran="| Yes  |";
 countMigrationsBefore=`grep -o "$Ran" <<< "$migrationsStatus" | wc -l`
@@ -7,6 +11,7 @@ echo "Ran migrations: "$countMigrations;
 
 git pull 
 
+# Count migrations before git pull
 migrationsStatus=$(php artisan migrate:status)
 ran="| Yes  |";
 countMigrationsAfter=`grep -o "$Ran" <<< "$migrationsStatus" | wc -l`
@@ -15,7 +20,10 @@ echo "Ran migrations: "$countMigrationsAfter;
 difference=$(( $countMigrationsBefore - $countMigrationsAfter ))
 echo "Difference: "$difference
 
+# Detect rollback
 if [ $difference < 0 ]; then
     echo "php artisan migrate:rollback --step="$difference;
     php artisan migrate:rollback --step=$difference
+    # Migrate to safety
+    php artisan migrate
 fi
